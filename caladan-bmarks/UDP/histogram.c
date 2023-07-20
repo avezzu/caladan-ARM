305db803787a35904f15692081ea296d71cc0716
Alessandro Vezzù <80411715+avezzu@users.noreply.github.com>
2023-07-20 15:38:19 +0200

    Update histogram.c

--- caladan-bmarks/UDP/histogram.c
+++ caladan-bmarks/UDP/histogram.c
@@ -98,9 +98,8 @@ static void *read_from(void *connection)
         }
     }
 
-    if (!atomic64_read(&done))
+    if (!(atomic64_add_and_fetch(&done, 1) - 1))
     {
-        atomic64_add_and_fetch(&done, 1);
         waitgroup_done(&wg);
     }
 
